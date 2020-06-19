99db5d5d4b686edcdee0660efb3fc2c72f4ce025
Ignacio San Martin <ignaciosm@gmail.com>
2020-06-19 10:52:38 -0500

    count_th done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,25 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    if word.find('th') < 0:
+        # print('not found', word.find('th'))
+        return 0
+
+    count = 0
+
+    if word.find('th') >= 0:
+        # print('found in word', word.find('th'))
+        word = word.replace('th', 'xx', 1)
+        # print('new_word:', word)
+        count += 1
+        # print('count', count)
+
+    return count_th(word) + count
+
+
+test = 'abcthxyz'
+print(count_th(test))
